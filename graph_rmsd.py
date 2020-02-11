3412fe998aeb81d63e5b7a75e64d52fa71b846f0
Miro-Astore <miro.astore@gmail.com>
2020-02-12 10:48:19 +1100

    going well just need to update graph functionality so it works reasonably

--- graph_rmsd.py
+++ graph_rmsd.py
@@ -1,14 +1,9 @@
 import numpy as np
+import os 
 
 import MDAnalysis as mda
 from matplotlib import pyplot as plt 
 import matplotlib.gridspec as gridspec
-
-plt.figure(figsize=(8,7))
-numblocks=1
-data=np.loadtxt('rmsd.dat')
-num_steps_i37r=len(data[:,3])
-timepoints=(num_steps_i37r)
 def chunkIt(seq, num):
     avg = len(seq) / float(num)
     out = []
@@ -18,63 +13,69 @@ def chunkIt(seq, num):
         last += avg
     return out
 
-gridspec.GridSpec(3,1)
-plt.subplot2grid((3,1),(0,0),colspan=1,rowspan=1)
-data = np.loadtxt('../WT/open/rmsd.dat')
-resnum=len(data[0,3:-1])
-x = data[:,0]*0.4
-y = data[:,3]
-plt.plot(x,y,label='WT')
-plt.title('RMSD of the lasoo domain in the wild type and the I37R mutant')
-plt.xlabel('time (ns)')
-plt.ylabel('lasoo domain RMSD ($\AA$)')
-data = np.loadtxt('rmsd.dat')
-resnum=len(data[0,3:-1])
-x = data[:,0]*0.4
-y = data[:,3]
-plt.plot(x,y,label='I37R')
-plt.legend()
+cwd=os.getcwd()
+system_list=np.loadtxt('system_list',dtype=str)
+num_systems=len(system_list)
 
-plt.subplot2grid((3,1),(1,0),colspan=1,rowspan=1)
-data = np.loadtxt('../WT/open/rmsd.dat')
-resnum=len(data[0,3:-1])
-blocks=chunkIt(range(timepoints),numblocks)
+plt.figure(figsize=(10,5))
+numblocks=1
+sys_count=1
+gridspec.GridSpec(num_systems+1,1)
+plt.subplot2grid((num_systems+1,1),(0,0),colspan=1,rowspan=1)
 
-width = 0.9
-inds = np.array(range(1,len(data[0,3:-1])+1))
+for i in system_list:
+    os.chdir(i)
+    sys_name=i.split('/')[-2].upper()
+    print (sys_name)
+    data = np.loadtxt('rmsd.dat')
+    x = data[:,1]*0.001
+    y = data[:,3]
+    plt.plot(x,y,label=sys_name)
+    plt.title('RMSD of the lasoo domain in the wild type and the I37R mutant')
+    plt.xlabel('time (ns)')
+    plt.ylabel('lasoo domain RMSD ($\AA$)')
+    plt.legend()
 
-for j in range(numblocks):
-    block=blocks[j]
-    resrmsd=np.zeros(resnum)
-    for i in range(resnum):
-        print (4+i)
-        resrmsd[i]=np.mean(data[block,4+i])
-    plt.bar(inds+j*width,resrmsd,width)
-    plt.ylim([0,6])
-plt.title('WT residue specific lasoo domain RMSDs (first 600ns)')
-plt.ylabel('Residue specific RMSDs $(\AA)$')
-plt.xlabel('residue number')
+color_vec=['blue']*70
+color_vec[36]='orange'
+for i in system_list:
+    os.chdir(i)
+    sys_name=i.split('/')[-2].upper()
+    print (sys_name)
+    data = np.loadtxt('rmsd.dat')
+    resnum=len(data[0,3:-1])
+    x = data[:,1]*0.001
+    y = data[:,3]
+    resnum=len(data[0,3:-1])
 
-data = np.loadtxt('rmsd.dat')
-resnum=len(data[0,3:-1])
-x = data[:,0]*0.4
-y = data[:,3]
+    plt.subplot2grid((num_systems+1,1),(sys_count,0),colspan=1,rowspan=1)
+    data = np.loadtxt('rmsd.dat')
+    resnum=len(data[0,3:-1])
+    blocks=chunkIt(np.array(data[:,0]),numblocks)
+    blocks=np.array(blocks).astype(int)
 
+    width = 0.9
+    inds = np.array(range(1,len(data[0,3:-1])+1))
 
-plt.subplot2grid((3,1),(2,0),colspan=1,rowspan=1)
-blocks=chunkIt(range(timepoints),numblocks)
-for j in range(numblocks):
-    block=blocks[j]
-    resrmsd=np.zeros(resnum)
-    for i in range(resnum):
-        resrmsd[i]=np.mean(data[block,4+i])
-    plt.bar(inds+j*width,resrmsd,width)
-    plt.ylim([0,6])
-plt.title('I37R residue specific lasoo domain RMSDs')
-plt.ylabel('Residue specific RMSDs $(\AA)$')
-plt.xlabel('residue number')
+    for j in range(numblocks):
+        block=blocks[j]
+        resrmsd=np.zeros(resnum)
+        for i in range(resnum):
+            resrmsd[i]=np.mean(data[block,4+i])
+        plt.bar(inds+j*width,resrmsd,width,color=color_vec)
+        #plt.ylim([0,6])
+        
+    plt.title(sys_name + ' residue specific lasoo domain RMSDs ')
+    plt.ylabel('Residue specific RMSDs $(\AA)$')
+    plt.xlabel('residue number')
 
+    data = np.loadtxt('rmsd.dat')
+    resnum=len(data[0,3:-1])
+    x = data[:,1]*0.001
+    y = data[:,3]
+    sys_count=sys_count+1
 
+os.chdir(cwd)
 plt.tight_layout()
 plt.savefig('fig.pdf')
 
