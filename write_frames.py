9ac6ad212e95db5817eacce477df01b7b9c0d4be
Miro Astore <miro.astore@gmail.com>
2023-04-07 13:45:31 -0400

    added selection string to write script

--- write_frames.py
+++ write_frames.py
@@ -1,14 +1,16 @@
 import sys
+import argparse
 import MDAnalysis as mda
 
 # usage 
-# python write_frames.py <PSFFILE> <TRAJFILE> 
+# python write_frames.py <PSFFILE> <TRAJFILE> <selection_string>
 #TODO
+
 
 
 u=mda.Universe(str(sys.argv[1]), sys.argv[2])
 file_root = str(sys.argv[2])[:-4]
-atoms = u.select_atoms('all')
+atoms = u.select_atoms(str(sys.argv[3]))
 frame_num = 0 
 for ts in u.trajectory:
     with mda.Writer(file_root + str(frame_num) + '.pdb',atoms.n_atoms) as W:
