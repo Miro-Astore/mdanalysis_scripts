51943675b7d871b7fd182fa2c92cec0630e89fe5
Miro Astore <miro.astore@gmail.com>
2023-04-07 13:41:59 -0400

    added script to write out every frame of a trajectory

---
+++ write_frames.py
@@ -0,0 +1,16 @@
+import sys
+import MDAnalysis as mda
+
+# usage 
+# python write_frames.py <PSFFILE> <TRAJFILE> 
+#TODO
+
+
+u=mda.Universe(str(sys.argv[1]), sys.argv[2])
+file_root = str(sys.argv[2])[:-4]
+atoms = u.select_atoms('all')
+frame_num = 0 
+for ts in u.trajectory:
+    with mda.Writer(file_root + str(frame_num) + '.pdb',atoms.n_atoms) as W:
+        W.write(atoms)
+        frame_num = frame_num + 1
