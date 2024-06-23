7975e550ecf9b44e8749b9cddb71191cfb7a6e8a
Miro Astore <miro.astore@gmail.com>
2024-06-23 17:19:49 -0400

    updates to rmsd and plotting graphing and fixed res_specific rmsd to take multiple selections

--- graph_dist.py
+++ graph_dist.py
@@ -1,61 +1,45 @@
 import numpy as np
-import matplotlib.pyplot as plt 
+import os 
+
+import MDAnalysis as mda
+from matplotlib import pyplot as plt 
 import matplotlib.gridspec as gridspec
 
-data_arr = [ 'flooded_wt/_rds_ma2374_flooding_CFTR_steer_open_candidate_1_pca_1_2_R334_E1126.dat', 'flooded_wt/_scratch_f91_ma2374_flooding_CFTR_steer_open_test_stability_R334_E1126_2.dat','flooded_wt/_scratch_f91_ma2374_flooding_CFTR_steer_open_test_stability_R334_E1126_5.dat']
+cwd=os.getcwd()
+#system_list=np.loadtxt('system_list',dtype=str)
+#system_list=['R347P_distance_1.dat','R347P_distance_2.dat','R347P_distance_3.dat']
+system_list=['WT_distance_1.dat','WT_distance_2.dat','WT_distance_3.dat']
+colors=['orange','tab:blue','red','yellow']
 
-cutoff = 500
-touching_dist=4
-touching_proportion = np.zeros(len(data_arr))
+num_systems=len(system_list)
 
-legend_arr = [ 'simulation 1', 'simulation 2','simulation 3']
-color_arr = ['tab:blue','green','red'] 
+plt.figure(figsize=(5,5))
+numblocks=1
+sys_count=1
 
-plt.figure(figsize=(10,5))
-temp_max=0
+for i in range(len(system_list)):
+    sys_name=i
+    print (sys_name)
+    data = np.loadtxt(system_list[i])
+    print(data)
+    x = data[:,0]
+    y = data[:,-1]
+    plt.plot(x,y,label='simulation ' + str(sys_name),color=colors[i])
 
-for i in range(len(data_arr)):
-    print(i)
-    data=np.loadtxt(data_arr[i])[0::2,:]
-    a_max=np.max(data[:,1])
-    if temp_max < a_max:
-        temp_max=a_max
-    
-    
-
-
-for i in range(len(data_arr)):
+plt.ylim([8,18])
+plt.xlim([0,2000])
 
 
-    data=np.loadtxt(data_arr[i])
-
-    data=np.array(data)
-    data=data.reshape((len(data),2))
-
-    x=data[0:cutoff,0]
-    y=data[0:cutoff,1]
-    touching_proportion[i] = np.sum(y<touching_dist)/len(y)
-
-    print(len(x))
-
-    plt.plot(x,y,label=legend_arr[i],color=color_arr[i], linewidth=0.6)
-
-touching_mean = round(np.mean(touching_proportion)*100,1)
-touching_std = round(np.std(touching_proportion)*100,1)
-
-plt.plot([0,cutoff],[touching_dist,touching_dist], 'k--', linewidth=1.0)
-
-plt.ylabel('Distance ($\mathrm{\AA}$)',fontsize=16)
-plt.xlabel('time (ns)',fontsize=16)
-plt.xticks(fontsize=14)
+#plt.title('RMSD of the lasoo domain in the wild type and the I37R mutant')
+plt.xlabel('Time (ns)',fontsize=16)
+plt.ylabel('CA Distance R347 - 924 ($\AA$)',fontsize=16)
 plt.yticks(fontsize=14)
-plt.legend(prop={'size':12})
+plt.xticks(fontsize=14)
+plt.legend(fontsize=14)
 
-plt.xlim([0,cutoff])
-plt.ylim([0,temp_max])
-text_string = "Salt bridge contact proportion " + str(touching_mean) + "$\mathrm{\pm}$ "  + str(touching_std) + "%"
 
-plt.text (1.0,0.5,s=text_string,fontsize=16)
+#os.chdir(cwd)
 plt.tight_layout()
-plt.savefig('wt_flooded_E1126_R334.pdf')
+plt.savefig('WT_distance.pdf')
 plt.show()
+
--- graph_rmsd.py
+++ graph_rmsd.py
@@ -7,31 +7,38 @@ import matplotlib.gridspec as gridspec
 
 cwd=os.getcwd()
 #system_list=np.loadtxt('system_list',dtype=str)
-system_list=['../1/sum_rmsd_nbd2.dat','../2/sum_rmsd_nbd2.dat','../3/sum_rmsd_nbd2.dat']
-colors=['orange','tab:blue','red']
+#$system_list=['WT_rmsd_tm6_1.dat','WT_rmsd_tm6_2.dat','WT_rmsd_tm6_3.dat']
+system_list=['R347P_rmsd_tm6_1.dat','R347P_rmsd_tm6_2.dat','R347P_rmsd_tm6_3.dat']
+colors=['orange','tab:blue','red','yellow']
 
 num_systems=len(system_list)
 
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(5,5))
 numblocks=1
 sys_count=1
 
 for i in range(len(system_list)):
-    sys_name=system_list[i].split('_')[-3].upper()
+    sys_name=i
     print (sys_name)
     data = np.loadtxt(system_list[i])
     x = data[:,1]*0.001
-    y = data[:,2]
-    plt.plot(x,y,label=sys_name,color=colors[i])
+    y = data[:,-1]
+    plt.plot(x,y,label='simulation ' + str(sys_name),color=colors[i])
+
+plt.ylim([0,5])
+plt.xlim([0,2000])
+
 
 #plt.title('RMSD of the lasoo domain in the wild type and the I37R mutant')
-plt.xlabel('time (ns)')
-plt.ylabel('Transmembrane RMSD ($\AA$)')
-plt.legend()
+plt.xlabel('Time (ns)',fontsize=16)
+plt.ylabel('TM6 RMSD ($\AA$)',fontsize=16)
+plt.yticks(fontsize=14)
+plt.xticks(fontsize=14)
+plt.legend(fontsize=14)
 
 
 #os.chdir(cwd)
 plt.tight_layout()
-plt.savefig('fig.pdf')
+plt.savefig('R347P.pdf')
 plt.show()
 
